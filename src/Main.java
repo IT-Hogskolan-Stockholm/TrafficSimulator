f93f5acecef01b0a7cb81292e2d20f06a614bb5d
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-09-27 12:04:31 +0200

    Added file 'test/TestMain.java' AND foleder 'out'

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@
  * The Main class serves as the entry point for the Traffic Simulator application.
  * It initializes a TrafficSimulator instance and starts the traffic simulation.
  */
-public class Main {
+public class Main   {
     public static void main(String[] args) {
         TrafficSimulator simulator = new TrafficSimulator();
         simulator.simulateTraffic();
---
+++ test/TestMain.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestMain {
+    @Test
+    void lookMain() {
+        assertEquals(5, 3+2);
+    }
+}
