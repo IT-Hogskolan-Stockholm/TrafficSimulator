bed15d2545eb53812e333b9c4e0ad533022badbc
khatch2_name <20730681+khatch2@users.noreply.github.com>
2024-09-27 15:39:26 +0200

    Några änderingar

--- test/TestCar.java
+++ test/TestCar.java
@@ -3,8 +3,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class TestCar {
     @Test
-    getTypeTest(){
-        Car car = new Car();
+    void getTypeTest(){
+        Car car = new Car("AbC457");
         assertEquals(car.getType(), "Car");
     }
 }
